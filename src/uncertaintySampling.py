9827ac36bf14c4921066121892e13f91b58eca6f
Zain ul abdeen <iamzainulabdeen@gmail.com>
2020-07-21 23:02:17 +0200

    Added Least confidence and Margin sampling

--- src/uncertaintySampling.py
+++ src/uncertaintySampling.py
@@ -8,7 +8,6 @@ import shutil
 
 class Images:
 
-
     def loadImages(self, paths):
         images = []
         fileNames = []
@@ -34,17 +33,23 @@ class Images:
         if(technique == 'uncertaintySampling'):
 
             for image in images:  # calculate entropies for all images
-                classes, counts = np.unique(image, return_counts=True)
+                _, counts = np.unique(image, return_counts=True)
                 score.append(entropy(counts))
 
-        chosenImages = sorted(zip(fileNames,score), key=lambda x: x[1])
+        chosenImages = sorted(zip(fileNames, score), key=lambda x: x[1])
 
-        return chosenImages #TODO----->Still need to chose how many images
+        return chosenImages  # TODO----->Still need to chose how many images
 
     def updateDatabase(self, chosenImages):
         source = 'Path of new images'
         destination = 'Path of old images'
 
         for image in chosenImages:
-            shutil.move(os.path.join(source,image[0]), destination)
-            
+            shutil.move(os.path.join(source, image[0]), destination)
+
+    def pred_probability(counts):
+        prob = []
+        for sample in counts:
+            n_classes = len(sample)
+            prob.append(sample/n_classes)
+        return prob
